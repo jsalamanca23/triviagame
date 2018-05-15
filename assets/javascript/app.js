e0152837d0ccfdab0d09e8c5eca32c664d0b3143
Jose Salamanca <jsalamanca228@gmail.com>
2018-05-14 23:41:25 -0700

    started on js

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var question1 = document.trivia.question1.value;
+var question2 = document.trivia.question2.value;
+var question3 = document.trivia.question3.value;
+var question4 = document.trivia.question4.value;
+var question5 = document.trivia.question5.value;
+var correct = 0;
+
+
+        if (question1 == "Daily Bugle") {
+            correct++;
+        }
+        if (question2 == "Baxter Building") {
+            correct++;
+        }
+        if (question3 == "Toothgrinder and Toothgnasher) {
+            correct++;
+        }
+        if (question4 == "WWII") {
+            correct++;
+        }
+        if (question5 == "Reed Richards") {
+            correct++;
+        }
